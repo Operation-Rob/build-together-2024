7b08ab73f02f394fae3ddd98edbba95069666d28
Karam <thethyka@gmail.com>
2024-06-08 16:35:00 +0800

    add imports

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,12 @@
 import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { json, useLoaderData } from '@remix-run/react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Country } from 'react-phone-number-input';
+import { Button } from '~/components/ui/button';
+import { PhoneInput } from '~/components/ui/phone-inputs';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '~/components/ui/select';
+import { tasks } from '~/lib/task';
 
 export async function loader({ context }: LoaderFunctionArgs) {
 	return json({
