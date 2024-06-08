19af5e2113114e6c1c3452687df23f8689bb7650
Karam <thethyka@gmail.com>
2024-06-08 18:08:24 +0800

    added transcript

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import type { Country } from 'react-phone-number-input';
 
 import Map from '~/components/Map';
+import Transcript from '~/components/Transcript';
 
 import { Button } from '~/components/ui/button';
 import { PhoneInput } from '~/components/ui/phone-inputs';
@@ -16,62 +17,62 @@ export async function loader({ context }: LoaderFunctionArgs) {
 		content: { hello: 'world' },
 	});
 }
-
 export default function Index() {
 	const form = useForm();
 	const onSubmit = () => {};
 	const [_, setCountry] = useState<Country>();
 	const [phoneNumber, setPhoneNumber] = useState('');
+  return (
+    <div className="grid h-screen grid-cols-2 grid-rows-2 overflow-hidden">
+      <div className="m-9">
+        <div>
+          <h1 className="text-4xl lg:text-5xl tracking-tight">
+            Start your Simulation 🚑
+          </h1>
+          <p className="my-6 leading-7">
+            When you're ready to get started, select a persona and enter your
+            phone number below.
+          </p>
+        </div>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col items-start space-y-8"
+        >
+          <div className="grid grid-cols-2 gap-5">
+            <PhoneInput
+              name="phoneNumber"
+              value={phoneNumber}
+              onChange={v => setPhoneNumber(v?.toString() ?? '')}
+              onCountryChange={setCountry}
+              placeholder="Enter a phone number"
+            />
+            <Select name="persona">
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a persona" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select a persona</SelectLabel>
+                  {Object.entries(tasks).map(([id, task]) => (
+                    <SelectItem key={id} value={id}>
+                      {task.title}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+      <div className="border-4 border-indigo-500 col-span-1 row-span-2 overflow-hidden">
+        <Map />
+      </div>
+	  <div className="">
+		
+        <Transcript callId="69"/>
 
-	return (
-		<div className="grid h-screen grid-cols-2">
-			<div className="m-9 h-full ">
-				<h1 className="scroll-m-20 text-4xl  tracking-tight lg:text-5xl">
-					Start your Simulation 🚑
-				</h1>
-				<p className="my-6 leading-7 ">
-					When you're ready to get started, select a persona and enter your
-					phone number below.
-				</p>
-
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className="flex flex-col items-start space-y-8"
-				>
-					<div className="grid grid-cols-2 gap-5">
-						<PhoneInput
-							name="phoneNumber"
-							value={phoneNumber}
-							onChange={v => setPhoneNumber(v?.toString() ?? '')}
-							onCountryChange={setCountry}
-							placeholder="Enter a phone number"
-						/>
-						<Select name="persona">
-							<SelectTrigger className="w-[180px]">
-								<SelectValue placeholder="Select a persona" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									<SelectLabel>Select a persona</SelectLabel>
-									{Object.entries(tasks).map(([id, task]) => {
-										return (
-											<SelectItem key={id} value={id}>
-												{task.title}
-											</SelectItem>
-										);
-									})}
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-					</div>
-					<Button type="submit">Submit</Button>
-				</form>
-			</div>
-			<div className="h-full border-4 border-indigo-500/100">
-				<Map />
-
-
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 }
